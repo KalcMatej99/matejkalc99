c554b792221175d4e22b8611930337a43d568cb8
Matej Kalc <matej.kalc@medius.si>
2024-09-29 18:52:50 +0200

    Removed unecessary header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,20 +4,7 @@ import Link from 'next/link'
 export default function Header() {
     return (
     <>
-        <header className="full-container py-10">
-            <nav className="space-x-5 flex">
-                <Link href="/" className="hover:bg-gray-400 rounded py-1 px-2">
-                    Home
-                </Link>
-                <Link href="#projects" className="hover:bg-gray-400 rounded py-1 px-2">
-                    Projects
-                </Link>
-                <Link href="mailto:matejkalc@gmail.com" className="hover:bg-gray-400 rounded py-1 px-2">
-                    Contact me
-                </Link>
-            </nav>
-            <hr className="mt-4 opacity-20" />
-        </header>
+        <header className="full-container py-10"></header>
     </>
     )
 }
