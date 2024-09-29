81b8bd0cebda4eaff93f93ded4b6bca782a8b4cb
Matej Kalc <matej.kalc@medius.si>
2024-09-29 19:51:30 +0200

    Removed unused links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,70 +9,68 @@ import Image from 'next/image'
 
 
 export default function HomePage() {
-    return  (
-    <>
-      <Head>
-            <link rel="shortcut icon" href="https://media.discordapp.net/attachments/870311702347018250/884454889240006716/Bugs_Banny-AdemCan.jpg" />
-            <title>Home - Matej Kalc</title>
-            <meta name="description" content="Data scientist" />
-            <meta name="keywords" content="Matej Kalc, ML, AI, Data science" />
-            <meta name="theme-color" content="#41B883"/>
-            <meta property="og:image"content="https://media.discordapp.net/attachments/870311702347018250/884454889240006716/Bugs_Banny-AdemCan.jpg" />
-        </Head>
+    return (
+        <>
+            <Head>
+                <title>Home - Matej Kalc</title>
+                <meta name="description" content="Data scientist" />
+                <meta name="keywords" content="Matej Kalc, ML, AI, Data science" />
+                <meta name="theme-color" content="#41B883" />
+            </Head>
 
-        <main>
-        <div className="full-container flex">
-            <div className="space-y-4">
-                <h1 className="text-4xl font-semibold text-white"><Link href="/">Hi, I'm Matej Kalc!</Link></h1>
-                <p className="mt-5 font-normal leading-relaxed">
-                    Data scientist from Triest, Italy.
-                    <br/>
-                    I have about 4 years of experience with building machine learning applications.
-                    <br/>
-                    Projects I have worked on include computer vision, llm, graph neural networks and more.
-                    <br/>
-                    In my free time a ski and train calisthenics.
-                    <br/>
-                    Find me on:
-                </p>
-                <div className="grid grid-cols-2 gap-5 mt-4 sm:gird-cols-3 md:grid-cols-4">
-                    <div className="flex space-x-2 text-white items-center overflow-hidden">
-                        <Link href="https://www.linkedin.com/in/matej-kalc-8122b5164/" target="_blank" rel="noreferrer">
-                            <Image src={linkedinLogo} width={56} height={56} alt="Python logo" className="flex-shrink-0" />                
-                        </Link>
-                        <span className='lang font-medium'>LinkedIn</span>
-                    </div>
-                    <div className="flex space-x-2 text-white items-center overflow-hidden">
-                        <Link href="https://github.com/KalcMatej99" target="_blank" rel="noreferrer">
-                            <Image src={githubLogo} width={28} height={28} alt="Python logo" className="flex-shrink-0" />                
-                        </Link>
-                        <span className='lang font-medium'>Github</span>
+            <main>
+                <div className="full-container flex">
+                    <div className="space-y-4">
+                        <h1 className="text-4xl font-semibold text-white"><Link href="/">Hi, I'm Matej Kalc!</Link></h1>
+                        <p className="mt-5 font-normal leading-relaxed">
+                            Data scientist from Triest, Italy.
+                            <br />
+                            I have about 4 years of experience with building machine learning applications.
+                            <br />
+                            Projects I have worked on include computer vision, llm, graph neural networks and more.
+                            <br />
+                            In my free time a ski and train calisthenics.
+                            <br />
+                            Find me on:
+                        </p>
+                        <div className="grid grid-cols-2 gap-5 mt-4 sm:gird-cols-3 md:grid-cols-4">
+                            <div className="flex space-x-2 text-white items-center overflow-hidden">
+                                <Link href="https://www.linkedin.com/in/matej-kalc-8122b5164/" target="_blank" rel="noreferrer">
+                                    <Image src={linkedinLogo} width={56} height={56} alt="Python logo" className="flex-shrink-0" />
+                                </Link>
+                                <span className='lang font-medium'>LinkedIn</span>
+                            </div>
+                            <div className="flex space-x-2 text-white items-center overflow-hidden">
+                                <Link href="https://github.com/KalcMatej99" target="_blank" rel="noreferrer">
+                                    <Image src={githubLogo} width={28} height={28} alt="Python logo" className="flex-shrink-0" />
+                                </Link>
+                                <span className='lang font-medium'>Github</span>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
-            </div>
-        </div>
-        
-        <div className="full-container mt-10">
-            <Tech/>
-        </div>
+                <div className="full-container mt-10">
+                    <Tech />
+                </div>
 
-        <div className="full-container mt-10">
-            <Repo/>
-        </div>
+                <div className="full-container mt-10">
+                    <Repo />
+                </div>
 
-        <div className="full-container mt-10">
-            <Edu/>
-        </div>
-        </main>
+                <div className="full-container mt-10">
+                    <Edu />
+                </div>
+            </main>
 
-        <footer className="w-full bottom-0 text-white bg-gray-900 bg-opacity-50 flex justify-center">
-            <div className="py-3 px-3 mx-auto flex items-center sm:flex-row flex-col">
-                <p className="text-sm font-semibold sm:ml-2 sm:pl-4 sm:mt-1 mt-4">
-                    © 2024 Matej Kalc
-                </p>
-            </div>
-        </footer>
-    </>
+            <footer className="w-full bottom-0 text-white bg-gray-900 bg-opacity-50 flex justify-center">
+                <div className="py-3 px-3 mx-auto flex items-center sm:flex-row flex-col">
+                    <p className="text-sm font-semibold sm:ml-2 sm:pl-4 sm:mt-1 mt-4">
+                        © 2024 Matej Kalc
+                    </p>
+                </div>
+            </footer>
+        </>
     )
 } 
